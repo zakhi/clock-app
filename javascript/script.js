90652e606d69a6a63b8145d6daf38cd7696c2da2
Zakhi Verech <zakhi@omnisol.co.il>
2021-10-01 00:21:13 +0300

    Create initial script to set the time

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-let moreButton = document.querySelector(".more-button");
+const moreButton = document.querySelector(".more-button");
 
 moreButton.addEventListener("click", e => {
     document.querySelector(".quote").classList.toggle("hidden")
@@ -8,3 +8,20 @@ moreButton.addEventListener("click", e => {
     const buttonText = moreButton.querySelector("span")
     buttonText.textContent = buttonText.textContent === "More" ? "Less" : "More"
 })
+
+const timeData = await fetchTimeData()
+
+document.querySelector(".time").textContent = new Date(timeData.datetime).toLocaleTimeString("en-GB", {
+    hour: "2-digit", minute: "2-digit"
+})
+
+document.querySelector(".timezone").textContent = timeData.abbreviation
+document.getElementById("current-timezone").textContent = timeData.timezone
+document.getElementById("day-of-year").textContent = timeData.day_of_year
+document.getElementById("day-of-week").textContent = timeData.day_of_week
+document.getElementById("week-number").textContent = timeData.week_number
+
+async function fetchTimeData() {
+    const response = await fetch("https://worldtimeapi.org/api/ip")
+    return await response.json()
+}
