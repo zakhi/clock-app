5bf0228fd611fa1b7bf9781e855327e43b61b802
Zakhi Verech <zakhi@omnisol.co.il>
2021-10-04 10:34:15 +0300

    Small fixes and refactoring

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,13 +2,14 @@ await Promise.all([fetchTimeData(), fetchCity()])
 
 document.querySelector(".clock").classList.remove("invisible")
 document.querySelector(".more-button").classList.remove("invisible")
-
+document.querySelector(".loading").classList.add("hidden")
 
 function fetchTimeData() {
     return fetch("https://worldtimeapi.org/api/ip")
         .then(response => response.json())
         .then(timeData => {
             const date = new Date(timeData.datetime);
+            const hours = date.getHours();
 
             document.querySelector(".time").textContent = date.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" })
             document.querySelector(".timezone").textContent = timeData.abbreviation
@@ -16,10 +17,9 @@ function fetchTimeData() {
             document.getElementById("day-of-year").textContent = timeData.day_of_year
             document.getElementById("day-of-week").textContent = timeData.day_of_week + 1
             document.getElementById("week-number").textContent = timeData.week_number
-            document.getElementById("simple-greeting").textContent = greetingBy(date.getHours())
-            document.querySelector(".greeting img").setAttribute("src", `assets/desktop/icon-${isDaytime(date.getHours()) ? "sun" : "moon"}.svg`)
-            document.querySelector("body").classList.toggle("evening", !isDaytime(date.getHours()))
-            document.querySelector(".loading").classList.add("hidden")
+            document.getElementById("simple-greeting").textContent = greetingBy(hours)
+            document.querySelector(".greeting img").setAttribute("src", `assets/desktop/icon-${isDaytime(hours) ? "sun" : "moon"}.svg`)
+            document.querySelector("body").classList.toggle("evening", !isDaytime(hours))
         })
 }
 
